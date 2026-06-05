84203b9032ac3d55f812d02469b925ac1a491a52
vhn0410 <vhn016260@gmail.com>
2026-06-05 10:32:53 +0700

    feat: integrate retriever as a knowledge tool and implement an api for user to chat with system

---
+++ backend/app/api/v1/endpoints/chat.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from app.schemas.chat_dto import ChatRequest, ChatResponse
+from app.application.chat.use_case import ChatUseCase
+
+router = APIRouter()
+chat_use_case = ChatUseCase()
+
+@router.post("/", response_model=ChatResponse)
+async def chat_with_farmmate(request: ChatRequest):
+    """
+    API gửi câu hỏi cho AI Nông nghiệp.
+    AI sẽ tự động dùng RAG Skill để tra cứu dữ liệu nếu cần.
+    """
+    response = await chat_use_case.process_chat(request)
+    return response
