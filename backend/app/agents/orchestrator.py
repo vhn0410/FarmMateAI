3283f75c3cbdaba9b08ed92c41154bb7b8854ee5
vhn0410 <vhn016260@gmail.com>
2026-06-05 21:56:07 +0700

    refactor: Rewrite openai_client using DI technique

--- backend/app/agents/orchestrator.py
+++ backend/app/agents/orchestrator.py
@@ -2,21 +2,24 @@ from langchain_classic.agents.agent_types import AgentType
 from langchain_classic.agents.initialize import initialize_agent
 from langchain_core.tools import Tool
 from app.agents.skills.rag_agriculture.tool import AgricultureRAGSkill
-from app.infrastructure.llm.openai_client import get_llm
 from app.infrastructure.vector_store.pgvector_provider import PGVectorProvider
+from app.infrastructure.llm.openai_client import OpenAIClient
 
 
 def get_chat_agent():
     """Khởi tạo và cấu hình AI Agent với các kỹ năng (Skills)."""
 
     # 1. Khởi tạo mô hình ngôn ngữ
-    llm = get_llm(model="gpt-4o-mini", temperature=0.0)
+    llm = OpenAIClient(model="gpt-4o-mini", temperature=0.0).get_llm()
 
     # 2. Khởi tạo Vector Store Provider
     vector_store_provider = PGVectorProvider()
 
     # 3. Khởi tạo các kỹ năng với Dependency Injection
-    rag_skill = AgricultureRAGSkill(vector_store_provider=vector_store_provider)
+    rag_skill = AgricultureRAGSkill(
+        vector_store_provider=vector_store_provider,
+        llm_provider=OpenAIClient(model="gpt-4o-mini", temperature=0.0),
+    )
 
     # 4. Đóng gói thành danh sách Tools cho Agent
     tools = [
